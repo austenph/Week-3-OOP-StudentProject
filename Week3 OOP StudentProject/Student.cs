e6305c0739ccecfba8a8f16de779ad53b97b1420
Austen Perelman-Hall <austenph@gmail.com>
2017-01-18 12:00:21 -0500

    added fields, properties, and started constructors.

--- Week3 OOP StudentProject/Student.cs
+++ Week3 OOP StudentProject/Student.cs
@@ -16,10 +16,42 @@ namespace Week3_OOP_StudentProject
         private string email;
         private int phone_number;
 
-
         //properties
+        public string FullName
+        {
+            get { return this.full_name; }
+            set { this.full_name = value; }
+        }
+
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+
+        public int PhoneNumber
+        {
+            get { return this.phone_number; }
+            set { this.phone_number = value; }
+        }
 
         //constructors
+        public Student ( )
+        {
+            this.full_name = "Jane Doe";
+            this.course = "no courses yet";
+            this.subject = "no subject yet";
+            this.university = "undefined university";
+            this.email = "janedoe@email.com";
+            this.phone_number = "000-000-0000";
+                }
+
+        public Student (string course, string subject, string university)
+        {
+            this.course = course;
+            this.subject = subject;
+            this.university = university;
+        }
 
         //methods
     }
