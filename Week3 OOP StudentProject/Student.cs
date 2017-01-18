5082b95d3d7d545ecebe9c655f55e58d1daf935c
Austen Perelman-Hall <austenph@gmail.com>
2017-01-18 11:49:53 -0500

    This is my first commit, in class project on OOP

---
+++ Week3 OOP StudentProject/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_OOP_StudentProject
+{
+    class Student
+    {
+        //fields
+        private string full_name;
+        private string course;
+        private string subject;
+        private string university;
+        private string email;
+        private int phone_number;
+
+
+        //properties
+
+        //constructors
+
+        //methods
+    }
+}
