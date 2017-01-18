b0640403c8762274bb3e45cd38e883a3fe7ddfed
Austen Perelman-Hall <austenph@gmail.com>
2017-01-18 13:23:13 -0500

    added a method to print out the students name, email, and phone number

--- Week3 OOP StudentProject/Program.cs
+++ Week3 OOP StudentProject/Program.cs
@@ -10,6 +10,9 @@ namespace Week3_OOP_StudentProject
     {
         static void Main(string[] args)
         {
+            Student newStudent = new Student("Baby Turtle", "babyturtle1@gmail.com", 1234567890);
+            newStudent.DisplayStudentContactInfo();
+
         }
     }
 }
--- Week3 OOP StudentProject/Student.cs
+++ Week3 OOP StudentProject/Student.cs
@@ -22,13 +22,26 @@ namespace Week3_OOP_StudentProject
             get { return this.full_name; }
             set { this.full_name = value; }
         }
-
+        public string Course
+        {
+            get { return this.course; }
+            set { this.course = value; }
+        }
+        public string Subject
+        {
+            get { return this.subject; }
+            set { this.subject = value; }
+        }
+        public string University
+        {
+            get { return this.university; }
+            set { this.university = value; }
+        }
         public string Email
         {
             get { return this.email; }
             set { this.email = value; }
         }
-
         public int PhoneNumber
         {
             get { return this.phone_number; }
@@ -36,15 +49,27 @@ namespace Week3_OOP_StudentProject
         }
 
         //constructors
-        public Student ( )
+        public Student()
         {
             this.full_name = "Jane Doe";
             this.course = "no courses yet";
             this.subject = "no subject yet";
             this.university = "undefined university";
             this.email = "janedoe@email.com";
-            this.phone_number = "000-000-0000";
-                }
+            this.phone_number = 0000000000;
+        }
+
+        public Student(string fullname)
+        {
+            this.full_name = fullname;
+        }
+
+        public Student(string fullname, string email, int phonenumber)
+        {
+            this.email = email;
+            this.phone_number = phonenumber;
+            this.full_name = fullname;
+        }
 
         public Student (string course, string subject, string university)
         {
@@ -53,6 +78,20 @@ namespace Week3_OOP_StudentProject
             this.university = university;
         }
 
+        public Student (string fullname, string course, string subject, string university, string email, int phonenumber)
+        {
+            this.email = email;
+            this.phone_number = phonenumber;
+            this.course = course;
+            this.subject = subject;
+            this.university = university;
+            this.full_name = fullname;
+        }
+
         //methods
+        public void DisplayStudentContactInfo()
+        {
+            Console.WriteLine("The students name is " + full_name + ", and email address is " + email + ", and phone number is " + phone_number + ".");
+        }
     }
 }
